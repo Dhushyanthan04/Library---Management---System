f4be472612a6a848902b8df755c10a89ee899f32
Dhushyanthan04 <dhushyanthang04@gmail.com>
2025-09-10 13:05:49 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+
+        public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Library Management System 🚀");
+        
+        Book book1 = new Book(1, "Java Basics");
+        Member member1 = new Member(101, "Priya");
+
+        System.out.println("Book: " + book1.getTitle());
+        System.out.println("Issued to: " + member1.getName());
+    }
+}
+    
+  
+    
