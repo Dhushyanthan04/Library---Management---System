93e9493e9a876651ceb3cc261bad51560a67d8e5
Dhushyanthan04 <dhushyanthang04@gmail.com>
2025-09-10 13:35:36 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,29 @@
-
-        public class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Library Management System 🚀");
-        
-        Book book1 = new Book(1, "Java Basics");
-        Member member1 = new Member(101, "Priya");
+        Library library = new Library();
 
-        System.out.println("Book: " + book1.getTitle());
-        System.out.println("Issued to: " + member1.getName());
+        Book book1 = new Book(1, "Java Basics");
+        Book book2 = new Book(2, "Data Structures");
+        library.addBook(book1);
+        library.addBook(book2);
+
+        Member member1 = new Member(101, "Priya");
+        Member member2 = new Member(102, "Dhushy");
+
+        library.showAvailableBooks();
+
+        System.out.println("\n--- Issuing Books ---");
+        library.issueBook(book1, member1); // Success
+        library.issueBook(book1, member2); // Already issued
+
+        System.out.println("\n--- Available Books After Issue ---");
+        library.showAvailableBooks();
+
+        System.out.println("\n--- Returning Books ---");
+        library.returnBook(book1, member1); // Success
+        library.returnBook(book2, member2); // Not borrowed
+
+        System.out.println("\n--- Available Books After Return ---");
+        library.showAvailableBooks();
     }
 }
-    
-  
-    
